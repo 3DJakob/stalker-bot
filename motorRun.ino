d180722d786574107b691207a87edcadb205f078
Jakob Unnebäck <jakob@folkdatorn.se>
2016-10-27 09:05:14 +0200

    Commented program

--- motorRun.ino
+++ motorRun.ino
@@ -1,28 +1,32 @@
-void motorRun (int direction) {
-  if (direction == 1) {
+// Motor run function!
+// This motor function steps the motorSpeed in to the desired value i.e. stop, forward, backward.
+// It uses a value between -255 & 255 to declare how fast and wich direction it's going.
+
+void motorRun (int direction) { // Declare direction value ( 0=stop 1=forward 2=backward)
+  if (direction == 1) { // If motor direction is forward
     if (motorSpeed < 255){
-      motorSpeed = lastMotorSpeed + step;
+      motorSpeed = lastMotorSpeed + step; // If motor is not 255 speed increase speed
     }
-  } else if (direction == 2) {
+  } else if (direction == 2) {  // If motor direction is backward
     if (motorSpeed > -255){
-      motorSpeed = lastMotorSpeed - step;
+      motorSpeed = lastMotorSpeed - step; // If motor is not -255 speed decrease speed
     }
-  } else {
+  } else { // motor direction is stop
     if (motorSpeed > 0){
-      motorSpeed = lastMotorSpeed - step;
+      motorSpeed = lastMotorSpeed - step; // If motorSpeed is positive decrease
     } else if (motorSpeed < 0) {
-      motorSpeed = lastMotorSpeed + step;
+      motorSpeed = lastMotorSpeed + step; // If motorSpeed is negative increase
     }
   }
 
-  if (motorSpeed > 0) {
+  if (motorSpeed > 0) { // If motorSpeed is positive run motor forward
     digitalWrite(Dir_b1, HIGH);
     digitalWrite(Dir_b2, LOW);
-    analogWrite(Pwm_b, motorSpeed);
-  } else {
+    analogWrite(Pwm_b, motorSpeed); // Run the motor at motorSpeed
+  } else {  // If motorSpeed is negative run motor backward
     digitalWrite(Dir_b1, LOW);
     digitalWrite(Dir_b2, HIGH);
-    analogWrite(Pwm_b, -motorSpeed);
+    analogWrite(Pwm_b, -motorSpeed); // Run the motor at motorSpeed
   }
-  lastMotorSpeed = motorSpeed;
+  lastMotorSpeed = motorSpeed;  // Set the lastMotorSpeed to the current
 }
--- stalker-bot.ino
+++ stalker-bot.ino
@@ -1,31 +1,32 @@
-typedef enum DriveStates {
+typedef enum DriveStates { // Defining possible DState values
   Init,
   Stop,
   Frwd,
   Bkwd
 };
 
-DriveStates DState, LastDState;
+DriveStates DState; // Assigning possible DState values
 
+// LED pins
 byte RedLed = 11;
 byte YellowLed = 10;
 byte GreenLed = 9;
 
-int motorSpeed = 0;
-int lastMotorSpeed = 0;
-int step = 5;
+int motorSpeed = 0; // Current motor speed variable
+int lastMotorSpeed = 0; // The last motor speed
+int step = 5; // The motor speed derivative
 
-byte DistanceSensorInput = 1;
-int DistanceSensorValue;
+byte DistanceSensorInput = 5; // Sensor input
+int DistanceSensorValue;  // Sensor value 0 - 1023
 
-byte Pwm_b = 3;
-byte Dir_b1 = 1;
-byte Dir_b2 = 0;
+byte Pwm_b = 3; // Motor pwm output
+byte Dir_b1 = 1;  // Direction 1 pin
+byte Dir_b2 = 0;  // Direction 2 pin
 
 void setup() {
-  DState = Init;
-  LastDState = Init;
+  DState = Init;  // Set the DState to init
 
+  // Configure inputs/outputs
   pinMode(RedLed, OUTPUT);
   pinMode(YellowLed, OUTPUT);
   pinMode(GreenLed, OUTPUT);
@@ -34,57 +35,55 @@ void setup() {
   pinMode(Dir_b1, OUTPUT);
   pinMode(Dir_b2, OUTPUT);
 
-  analogWrite(Pwm_b, 0);
-
-  Serial.begin(9600);
+  analogWrite(Pwm_b, 0); // Run motor at 0 speed
+  Serial.begin(9600); // Begin serial monitor at 9600 baud rate
 }
 
 void loop() {
-  DistanceSensorValue = analogRead(DistanceSensorInput);
-  DistanceSensorValue = analogRead(5);
+  DistanceSensorValue = analogRead(DistanceSensorInput);  // Read sensor
 
-  Serial.println(motorSpeed);
+  Serial.println(motorSpeed); // Log motorSpeed
 
-  switch (DState) {
-    case Init:
+  switch (DState) { // State selected by DState variable
+    case Init:  // If DState = Init => Turn on RedLed and wait 1s
       digitalWrite(YellowLed, LOW);
       digitalWrite(GreenLed, LOW);
       digitalWrite(RedLed, HIGH);
       delay(1000);
       Serial.println("starting");
       DState = Stop;
-    break;
+    break;  // End of case
 
-    case Stop:
+    case Stop:  // If DState = Stop => Turn on YellowLed and run motor function
       digitalWrite(YellowLed, HIGH);
       digitalWrite(GreenLed, LOW);
       digitalWrite(RedLed, LOW);
-      motorRun(0);
+      motorRun(0); // Run motor function, send value 0
       if (DistanceSensorValue < 170) {
-        DState = Frwd;
+        DState = Frwd;  // If DistanceSensorValue < 170 change DState to Frwd
       } else if (DistanceSensorValue > 230) {
-        DState = Bkwd;
+        DState = Bkwd;  // If DistanceSensorValue > 230 change DState to Bkwd
       }
-    break;
+    break;  // End of case
 
-    case Frwd:
+    case Frwd:  // If DState = Frwd => Turn on GreenLed and run motor function
       digitalWrite(YellowLed, LOW);
       digitalWrite(GreenLed, HIGH);
       digitalWrite(RedLed, LOW);
-      motorRun(1);
+      motorRun(1);  // Run motor function, send value 1
       if (DistanceSensorValue > 170) {
-        DState = Stop;
+        DState = Stop;  // If DistanceSensorValue > 170 change DState to Stop
       }
-    break;
+    break;  // End of case
 
-    case Bkwd:
+    case Bkwd:  // If DState = Bkwd => Turn on RedLed and run motor function
       digitalWrite(YellowLed, LOW);
       digitalWrite(GreenLed, LOW);
       digitalWrite(RedLed, HIGH);
-      motorRun(2);
+      motorRun(2);  // Run motor function, send value 2
       if (DistanceSensorValue < 230) {
-        DState = Stop;
+        DState = Stop;  // If DistanceSensorValue < 230 change DState to Stop
       }
-    break;
+    break;  // End of case
   }
 }
